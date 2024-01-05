5775650bb5ed898dbff7c3ec468e62b79f7f0bbc
jongmee <ra123221@gmail.com>
2024-01-06 02:32:03 +0900

    feat: 면접별 지원자 조회 api 생성

--- src/main/java/com/gotcha/server/applicant/controller/ApplicantController.java
+++ src/main/java/com/gotcha/server/applicant/controller/ApplicantController.java
@@ -1,16 +1,19 @@
 package com.gotcha.server.applicant.controller;
 
 import com.gotcha.server.applicant.dto.request.InterviewProceedRequest;
+import com.gotcha.server.applicant.dto.response.ApplicantsResponse;
 import com.gotcha.server.applicant.dto.response.InterviewProceedResponse;
 import com.gotcha.server.applicant.dto.response.TodayInterviewResponse;
 import com.gotcha.server.applicant.service.ApplicantService;
 import com.gotcha.server.auth.security.MemberDetails;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api")
@@ -27,5 +30,10 @@ public class ApplicantController {
     @GetMapping("/todays-interview")
     public ResponseEntity<TodayInterviewResponse> countInterview(@AuthenticationPrincipal final MemberDetails details) {
         return ResponseEntity.ok(applicantService.countTodayInterview(details));
+    }
+
+    @GetMapping("/applicants")
+    public ResponseEntity<List<ApplicantsResponse>> findAllApplicantByInterview(@RequestParam(name = "interview-id") Long interviewId) {
+        return ResponseEntity.ok(applicantService.listApplicantByInterview(interviewId));
     }
 }
--- src/main/java/com/gotcha/server/applicant/service/ApplicantService.java
+++ src/main/java/com/gotcha/server/applicant/service/ApplicantService.java
@@ -4,6 +4,7 @@ import com.gotcha.server.applicant.domain.Applicant;
 import com.gotcha.server.applicant.domain.Interviewer;
 import com.gotcha.server.applicant.domain.PreparedInterviewer;
 import com.gotcha.server.applicant.dto.request.InterviewProceedRequest;
+import com.gotcha.server.applicant.dto.response.ApplicantsResponse;
 import com.gotcha.server.applicant.dto.response.InterviewProceedResponse;
 import com.gotcha.server.applicant.dto.response.TodayInterviewResponse;
 import com.gotcha.server.applicant.repository.ApplicantRepository;
@@ -12,6 +13,9 @@ import com.gotcha.server.applicant.repository.PreparedInterviewerRepository;
 import com.gotcha.server.auth.security.MemberDetails;
 import com.gotcha.server.global.exception.AppException;
 import com.gotcha.server.global.exception.ErrorCode;
+import com.gotcha.server.project.domain.Interview;
+import com.gotcha.server.project.repository.InterviewRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +27,7 @@ public class ApplicantService {
     private final ApplicantRepository applicantRepository;
     private final InterviewerRepository interviewerRepository;
     private final PreparedInterviewerRepository preparedInterviewerRepository;
+    private final InterviewRepository interviewRepository;
 
     @Transactional
     public InterviewProceedResponse proceedToInterview(final InterviewProceedRequest request, final MemberDetails details) {
@@ -45,5 +50,11 @@ public class ApplicantService {
     public TodayInterviewResponse countTodayInterview(final MemberDetails details) {
         long count = interviewerRepository.countTodayInterview(details.member());
         return new TodayInterviewResponse(count);
+    }
+
+    public List<ApplicantsResponse> listApplicantByInterview(final Long interviewId) {
+        Interview interview = interviewRepository.findById(interviewId)
+                .orElseThrow(() -> new AppException(ErrorCode.INTERVIEW_NOT_FOUNT));
+        return applicantRepository.findAllByInterviewWithKeywords(interview);
     }
 }
