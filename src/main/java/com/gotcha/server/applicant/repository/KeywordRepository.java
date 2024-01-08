b83fefaf1974c04e106dc601896026a99806438b
jongmee <ra123221@gmail.com>
2024-01-08 19:26:47 +0900

    feat: 지원자 상세 정보 기능 구현

--- src/main/java/com/gotcha/server/applicant/repository/KeywordRepository.java
+++ src/main/java/com/gotcha/server/applicant/repository/KeywordRepository.java
@@ -1,8 +1,10 @@
 package com.gotcha.server.applicant.repository;
 
+import com.gotcha.server.applicant.domain.Applicant;
 import com.gotcha.server.applicant.domain.Keyword;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
-
+    List<Keyword> findAllByApplicant(Applicant applicant);
 }
