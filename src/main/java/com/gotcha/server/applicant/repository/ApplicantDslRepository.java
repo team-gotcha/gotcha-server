312a636d36b6ccc0fc24f24a1e86d38db3c28e75
jongmee <ra123221@gmail.com>
2024-01-10 20:00:35 +0900

    feat: 합격한 지원자 목록 조회하는 쿼리 생성

--- src/main/java/com/gotcha/server/applicant/repository/ApplicantDslRepository.java
+++ src/main/java/com/gotcha/server/applicant/repository/ApplicantDslRepository.java
@@ -1,9 +1,11 @@
 package com.gotcha.server.applicant.repository;
 
 import com.gotcha.server.applicant.dto.response.ApplicantsResponse;
+import com.gotcha.server.applicant.dto.response.PassedApplicantsResponse;
 import com.gotcha.server.project.domain.Interview;
 import java.util.List;
 
 public interface ApplicantDslRepository {
     List<ApplicantsResponse> findAllByInterviewWithKeywords(Interview interview);
+    List<PassedApplicantsResponse> findAllPassedApplicantsWithKeywords(Interview interview);
 }
