fbb452afa660fede997dbb51eeba530b1439181e
jongmee <ra123221@gmail.com>
2024-01-20 22:11:24 +0900

    test: 지원자 질문 공개를 한 명이라도 비동의했다면 변경할 수 없는지 테스트

--- src/test/java/com/gotcha/server/applicant/service/ApplicantServiceTest.java
+++ src/test/java/com/gotcha/server/applicant/service/ApplicantServiceTest.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.gotcha.server.applicant.domain.Applicant;
 import com.gotcha.server.applicant.domain.Interviewer;
 import com.gotcha.server.applicant.domain.KeywordType;
+import com.gotcha.server.applicant.dto.request.GoQuestionPublicRequest;
 import com.gotcha.server.applicant.dto.request.InterviewProceedRequest;
 import com.gotcha.server.applicant.dto.response.ApplicantResponse;
+import com.gotcha.server.applicant.repository.ApplicantRepository;
 import com.gotcha.server.auth.security.MemberDetails;
 import com.gotcha.server.common.IntegrationTest;
 import com.gotcha.server.member.domain.Member;
 import com.gotcha.server.project.domain.Interview;
 import com.gotcha.server.project.domain.Project;
 import com.gotcha.server.question.domain.IndividualQuestion;
+import com.gotcha.server.question.domain.QuestionPublicType;
 import com.gotcha.server.question.repository.IndividualQuestionRepository;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -26,6 +29,9 @@ class ApplicantServiceTest extends IntegrationTest {
 
     @Autowired
     private IndividualQuestionRepository individualQuestionRepository;
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
 
     @Test
     void 지원자_상세정보_조회하기() {
@@ -79,5 +85,26 @@ class ApplicantServiceTest extends IntegrationTest {
         // then
         List<IndividualQuestion> 등록된_개별질문들 = individualQuestionRepository.findAllDuringInterview(지원자A);
         assertThat(등록된_개별질문들).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("지원자의 질문 공개를 동의해도 이전에 비동의하였다면 변경되지 않는다")
+    void 질문공개_비동의시_변경불가() {
+        // given
+        Project 테스트프로젝트 = environ.테스트프로젝트_저장하기();
+        Interview 테스트면접 = environ.테스트면접_저장하기(테스트프로젝트, "테스트면접");
+        Applicant 지원자A = environ.테스트지원자_저장하기(테스트면접, "지원자A");
+
+        GoQuestionPublicRequest 비동의요청 = new GoQuestionPublicRequest(false);
+        GoQuestionPublicRequest 동의요청 = new GoQuestionPublicRequest(true);
+
+        // when
+        applicantService.makeQuestionsPublic(지원자A.getId(), 비동의요청);
+        applicantService.makeQuestionsPublic(지원자A.getId(), 동의요청);
+
+        // then
+        Applicant 변경된지원자 = applicantRepository.findById(지원자A.getId()).get();
+        assertEquals(QuestionPublicType.PENDING, 지원자A.getQuestionPublicType());
+        assertEquals(QuestionPublicType.PRIVATE, 변경된지원자.getQuestionPublicType());
     }
 }
