b6c2f278f253e3cae765a2afbbf3b1d128395ffa
jongmee <ra123221@gmail.com>
2024-01-22 23:00:49 +0900

    fix: swagger url 수정

--- src/main/java/com/gotcha/server/global/config/OpenApiConfig.java
+++ src/main/java/com/gotcha/server/global/config/OpenApiConfig.java
@@ -1,27 +1,37 @@
 package com.gotcha.server.global.config;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    private final String deployedUrl;
 
-    private static final String SECURITY_SCHEME_NAME = "authorization";
+    public OpenApiConfig(@Value("${deployed-url}") final String url) {
+        this.deployedUrl = url;
+    }
 
     @Bean
     public OpenAPI swaggerApi() {
         return new OpenAPI()
                 .components(new Components()
-                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
-                                .name(SECURITY_SCHEME_NAME)
+                        .addSecuritySchemes(AUTHORIZATION, new SecurityScheme()
+                                .name(AUTHORIZATION)
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .addSecurityItem(new SecurityRequirement().addList(AUTHORIZATION))
+                .servers(List.of(new Server().url(deployedUrl)))
                 .info(new Info()
                         .title("API Document")
                         .version("v1")
