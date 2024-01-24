c5d9ed1d4143e790a07499f496b43c0ef7d792c8
jongmee <ra123221@gmail.com>
2024-01-24 23:13:06 +0900

    feat: 지원자 목록 중 유저의 즐겨찾기 조회하는 jpa 메서드 추가

--- src/main/java/com/gotcha/server/applicant/repository/FavoriteRepository.java
+++ src/main/java/com/gotcha/server/applicant/repository/FavoriteRepository.java
@@ -3,9 +3,11 @@ package com.gotcha.server.applicant.repository;
 import com.gotcha.server.applicant.domain.Applicant;
 import com.gotcha.server.applicant.domain.Favorite;
 import com.gotcha.server.member.domain.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     Optional<Favorite> findByApplicantAndMember(Applicant applicant, Member member);
+    List<Favorite> findAllByMemberAndApplicantIn(Member member, List<Applicant> applicants);
 }
