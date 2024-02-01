24fc5ce538f0b7e945f627678ebbc1ed57016146
nzeong <stac_cato@naver.com>
2024-02-02 00:10:57 +0900

    hotfix: cors 설정 변경

--- src/main/java/com/gotcha/server/auth/config/SecurityConfig.java
+++ src/main/java/com/gotcha/server/auth/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
 
@@ -49,14 +50,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
-        return request -> {
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowedHeaders(Collections.singletonList("*"));
             config.setAllowedMethods(Collections.singletonList("*"));
             config.setAllowedOriginPatterns(Collections.singletonList("*"));
             config.setAllowCredentials(true);
-            return config;
-        };
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", config);
+            return source;
     }
 }
