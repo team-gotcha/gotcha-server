c5ac70a14d7fcaa43931b925626578eb86efbd17
nzeong <stac_cato@naver.com>
2024-02-02 01:24:55 +0900

    hotfix: spring security 설정 원상태로 변경

--- src/main/java/com/gotcha/server/auth/config/SecurityConfig.java
+++ src/main/java/com/gotcha/server/auth/config/SecurityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
 
@@ -38,12 +37,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
 //                        .requestMatchers("/post").authenticated()
 //                        .requestMatchers(HttpMethod.DELETE, "/post/*").authenticated()
-
-                        .requestMatchers(HttpMethod.GET,"/swagger-ui/**", "/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/login/google").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/refresh", "/api/logout").permitAll()
-                        .requestMatchers("/**").authenticated()
-                        .anyRequest().permitAll()
+                                .requestMatchers(HttpMethod.GET,"/swagger-ui/**", "/api-docs/**").permitAll()
+                                .anyRequest().permitAll()
                 );
 
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
@@ -51,16 +46,14 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
     CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
             CorsConfiguration config = new CorsConfiguration();
             config.setAllowedHeaders(Collections.singletonList("*"));
             config.setAllowedMethods(Collections.singletonList("*"));
             config.setAllowedOriginPatterns(Collections.singletonList("*"));
             config.setAllowCredentials(true);
-
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/**", config);
-            return source;
+            return config;
+        };
     }
 }
